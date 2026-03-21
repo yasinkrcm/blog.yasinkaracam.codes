32bcee710b7388fd7bf5f98ffde3ea336b4a434a
Yasin karaçam <yasinkaracam67@gmail.com>
2026-03-21 15:19:15 +0300

    icon

--- client/app/icon.tsx
+++ client/app/icon.tsx
@@ -1,26 +1,34 @@
-import Image from 'next/image';
+import { ImageResponse } from 'next/og'
 
+// Resim boyutu ve tipi
+export const size = {
+  width: 32,
+  height: 32,
+}
+export const contentType = 'image/png'
+
+// Varsayılan fonksiyon mutlaka bir çıktı döndürmeli
 export default function Icon() {
-  return (
-    <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-      <defs>
-        <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
-          <stop offset="0%" style={{stopColor: '#0066FF', stopOpacity: 1}} />
-          <stop offset="100%" style={{stopColor: '#0066FF', stopOpacity: 0.5}} />
-        </linearGradient>
-      </defs>
-      <text 
-        x="50%" 
-        y="50%" 
-        textAnchor="middle" 
-        dominantBaseline="middle" 
-        fontFamily="system-ui, -apple-system" 
-        fontWeight="bold" 
-        fontSize="16" 
-        fill="url(#gradient)"
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 24,
+          background: 'black',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'white',
+          borderRadius: '20%'
+        }}
       >
-        YK
-      </text>
-    </svg>
-  );
+        Y
+      </div>
+    ),
+    {
+      ...size,
+    }
+  )
 }
